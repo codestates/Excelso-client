1c27875ee8fe53b08435d66a9cf51d61b1204e95
ahnella04 <chqkq96@gmail.com>
2021-07-06 09:57:41 +0900

    [Create] MainPage && Styling Part1

---
+++ Excelso-client/src/components/footer/footerStyles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/macro";
+
+export const FootDiv = styled.div`
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 18rem;
+  background-color: black;
+  color: white;
+  text-align: center;
+`;

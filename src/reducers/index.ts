e8b9ebe258439c48b8c4f322f8f573d1c4aba9e2
ahnella04 <chqkq96@gmail.com>
2021-07-08 11:03:26 +0900

    [Client] BackUp

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,9 +3,12 @@ import { combineReducers } from "redux";
 import { allCoffeeReducer } from "./coffeeReducer";
 import { GetReviewReducer } from "./reviewReducer";
 
+import loginReducer from "./loginReducer";
+
 export const rootReducer = combineReducers({
   coffee: allCoffeeReducer,
   getRivew: GetReviewReducer,
+  loginReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

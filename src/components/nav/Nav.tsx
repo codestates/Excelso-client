defa2e7e35900e5c1a4c83845d8337043f1cea7b
ahnella04 <chqkq96@gmail.com>
2021-07-12 10:21:29 +0900

    [Client] Update: MainPage Part5

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
+import { Autoplay } from "swiper";
+
+import { BiMenu } from "react-icons/bi";
 
 import {
   NavContainer,
@@ -12,7 +15,13 @@ import {
   SignUpDiv,
   Button,
   Logo,
-  MypageButton
+  MypageButton,
+  Menubar,
+  MenuBarCol,
+  MenuHidden,
+  MenuLoginDiv,
+  MenuSignupDiv,
+  MenuHideButton
 } from "./navStyles";
 
 const Nav = () => {
@@ -21,6 +30,8 @@ const Nav = () => {
   const [clickLogin, setClickLogin] = useState(false);
   const [clickSignUp, setClickSignUp] = useState(false);
   const [clickLogout, setClickLogout] = useState(false);
+
+  const [menu, setMenu] = useState(false);
 
   const goHome = () => {
     console.log("goHome");
@@ -47,44 +58,94 @@ const Nav = () => {
     sessionStorage.clear();
   };
 
+  const onClickMenu = () => {
+    if (menu) {
+      setMenu(false);
+    } else {
+      setMenu(true);
+    }
+  };
+
   return (
-    <NavContainer>
-      <LogoDiv>
-        <Logo onClick={goHome}>Excelso</Logo>
-      </LogoDiv>
-      <BeanDiv>
-        <Link to="/brandinfo">
-          <MenuButton>원두</MenuButton>
-        </Link>
-      </BeanDiv>
-      <MenuDiv>
-        <Link to="/coffee">
-          <MenuButton>메뉴</MenuButton>
-        </Link>
-      </MenuDiv>
-      <LoginDiv>
-        {sessionStorage.getItem("accessToken") ? (
-          <Link to="/mypage">
-            <MypageButton>마이페이지</MypageButton>
+    <>
+      <NavContainer>
+        <LogoDiv>
+          <Logo onClick={goHome}>Excelso</Logo>
+        </LogoDiv>
+        <BeanDiv>
+          <Link to="/brandinfo">
+            <MenuButton>원두</MenuButton>
           </Link>
+        </BeanDiv>
+        <MenuDiv>
+          <Link to="/coffee">
+            <MenuButton>음료</MenuButton>
+          </Link>
+        </MenuDiv>
+        <LoginDiv>
+          {sessionStorage.getItem("accessToken") ? (
+            <Link to="/mypage">
+              <MypageButton>마이페이지</MypageButton>
+            </Link>
+          ) : (
+            <Link to="/login">
+              <Button onClick={onClickLogin}>로그인</Button>
+            </Link>
+          )}
+        </LoginDiv>
+        <SignUpDiv>
+          {sessionStorage.getItem("accessToken") ? (
+            <Link to="/">
+              <Button onClick={onClickLogout}>로그아웃</Button>
+            </Link>
+          ) : (
+            <Link to="/signup">
+              <Button onClick={onClickSignUp}>회원가입</Button>
+            </Link>
+          )}
+        </SignUpDiv>
+        <Menubar onClick={onClickMenu}>
+          <div>
+            <BiMenu size="lg"></BiMenu>
+          </div>
+        </Menubar>
+      </NavContainer>
+      <MenuHidden>
+        {menu ? (
+          <MenuBarCol>
+            <MenuLoginDiv>
+              {sessionStorage.getItem("accessToken") ? (
+                <Link to="/mypage">
+                  <MenuHideButton>마이페이지</MenuHideButton>
+                </Link>
+              ) : (
+                <Link to="/login">
+                  <MenuHideButton onClick={onClickLogin}>로그인</MenuHideButton>
+                </Link>
+              )}
+            </MenuLoginDiv>
+            <MenuSignupDiv>
+              {sessionStorage.getItem("accessToken") ? (
+                <Link to="/">
+                  <MenuHideButton onClick={onClickLogout}>
+                    로그아웃
+                  </MenuHideButton>
+                </Link>
+              ) : (
+                <Link to="/signup">
+                  <MenuHideButton onClick={onClickSignUp}>
+                    회원가입
+                  </MenuHideButton>
+                </Link>
+              )}
+            </MenuSignupDiv>
+          </MenuBarCol>
         ) : (
-          <Link to="/login">
-            <Button onClick={onClickLogin}>로그인</Button>
-          </Link>
+          ""
         )}
-      </LoginDiv>
-      <SignUpDiv>
-        {sessionStorage.getItem("accessToken") ? (
-          <Link to="/">
-            <Button onClick={onClickLogout}>로그아웃</Button>
-          </Link>
-        ) : (
-          <Link to="/signup">
-            <Button onClick={onClickSignUp}>회원가입</Button>
-          </Link>
-        )}
-      </SignUpDiv>
-    </NavContainer>
+        )
+      </MenuHidden>
+    </>
   );
 };
 
